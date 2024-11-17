5783be5887265495009414ea7704697ec4d28ec0
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-11-17 12:57:27 +0000

    count

---
+++ Basic Maths/countdigits.java
@@ -0,0 +1,19 @@
+public class countdigits{
+    private static int count(int n){
+        if(n<0){
+            n=n*-1;
+        }
+        int cnt=0;
+        while(n>0){
+            int lastdigit=n%10;
+            n=n/10;
+            cnt++;
+        }
+        return cnt;
+    }
+       
+        public static void main(String[] args){
+            countdigits c=new countdigits();
+            System.out.println(c.count(12345));
+        }
+    }
