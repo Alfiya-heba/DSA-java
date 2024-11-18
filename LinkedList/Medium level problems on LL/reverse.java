4239273dc4765ed37a1ac8d2c37464f03e42510b
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-11-18 15:29:40 +0000

    LL

--- LinkedList/Medium level problems on LL/reverse.java
+++ LinkedList/Medium level problems on LL/reverse.java
@@ -1,5 +1,52 @@
 // Brute force solution//
-import java.util.Stack;
+// import java.util.Stack;
+// class Node{
+//     int data;
+//     Node next;
+//     Node(int data1,Node next1){
+//         this.data=data1;
+//         this.next=next1;
+//     }
+//     Node(int data2){
+//         this.data=data2;
+//         this.next=null;
+//     }
+// };
+// public class reverse{
+//     private static Node reverseLL(Node head){
+//         Node temp=head;
+//         Stack<Integer> stack=new Stack<>();
+//         while(temp!=null){
+//             stack.push(temp.data);
+//             temp=temp.next;
+//         }
+//         temp=head;
+//         while(temp!=null){
+//             temp.data=stack.pop();
+//             temp=temp.next;
+//         }
+//         return head;
+//     }
+//     private static void print(Node head){
+//         Node temp=head;
+//         while(temp!=null){
+//             System.out.print(temp.data +" ");
+//             temp=temp.next;
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args){
+//         Node head=new Node(1);
+//         head.next=new Node(2);
+//         head.next.next=new Node(3);
+//         head.next.next.next=new Node(4);
+//         head.next.next.next.next=new Node(4);
+//         head=reverseLL(head);
+//         print(head);
+//     }
+// }
+
+// optimal solution//
 class Node{
     int data;
     Node next;
@@ -15,17 +62,14 @@ class Node{
 public class reverse{
     private static Node reverseLL(Node head){
         Node temp=head;
-        Stack<Integer> stack=new Stack<>();
+        Node prev=null;
         while(temp!=null){
-            stack.push(temp.data);
-            temp=temp.next;
+            Node front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
         }
-        temp=head;
-        while(temp!=null){
-            temp.data=stack.pop();
-            temp=temp.next;
-        }
-        return head;
+        return prev;
     }
     private static void print(Node head){
         Node temp=head;
@@ -40,7 +84,7 @@ public class reverse{
         head.next=new Node(2);
         head.next.next=new Node(3);
         head.next.next.next=new Node(4);
-        head.next.next.next.next=new Node(4);
+        head.next.next.next.next=new Node(5);
         head=reverseLL(head);
         print(head);
     }
