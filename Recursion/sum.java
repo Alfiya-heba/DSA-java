f74f1bb01e20cdd026b3f8eeb5e055e26d546c81
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-12-07 15:25:19 +0000

    sum

---
+++ Recursion/sum.java
@@ -0,0 +1,13 @@
+public class sum{
+    private static void sum(int i,int sum){
+        if(i<1){
+            System.out.println(sum);
+            return;
+        }
+        sum(i-1,sum+i);
+    }
+    public static void main(String[] args){
+        int n=5;
+        sum(n,0);
+    }
+}
